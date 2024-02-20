c5ce655014d36e0d5a1b890de982d6a73f38b3b1
xDShiro_Home <cmex-d@yandex.ru>
2024-02-19 19:50:15 -0500

    hopfs

--- knots_propagation/data_generation_hopfs_with_LG.py
+++ knots_propagation/data_generation_hopfs_with_LG.py
@@ -7,7 +7,7 @@ import csv
 import json
 from tqdm import trange
 
-SAMPLES = 1
+SAMPLES = 10
 indx_plus = 0
 
 plot = 0
@@ -18,11 +18,11 @@ print_values = 0
 centering = 0
 seed = None  # does work with more than 1 phase screen
 no_last_plane = True
-folder = 'data_basis'
+folder = 'data_low_10'
 # folder = 'delete'
 
 spectrum_save = 1
-no_turb = 1
+no_turb = 0
 
 # meshes and boundaries for getting a knot
 x_lim_3D_knot, y_lim_3D_knot, z_lim_3D_knot = (-7.0, 7.0), (-7.0, 7.0), (-2.0, 2.0)
@@ -47,8 +47,8 @@ screens_num2 = 1
 multiplier2 = [1] * screens_num2
 
 # turbulence
-Cn2 = 1.35e-13  # turbulence strength  is basically in the range of 10−17–10−12 m−2/3
-# Cn2 = 3.21e-14
+# Cn2 = 1.35e-13  # turbulence strength  is basically in the range of 10−17–10−12 m−2/3
+Cn2 = 3.21e-14
 # Cn2 = 3.21e-15
 # Cn2 = 3.21e-40
 # https://www.mdpi.com/2076-3417/11/22/10548
