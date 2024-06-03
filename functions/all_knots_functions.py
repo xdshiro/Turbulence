7712b8d834a0bfe8930138cfb8192bfca01d7da4
xDShiro <cmex-d@ya.ru>
2024-06-03 11:51:26 -0400

    все домой

--- functions/all_knots_functions.py
+++ functions/all_knots_functions.py
@@ -1445,6 +1445,63 @@ def trefoil_standard_16(mesh_3D, braid_func=braid, modes_cutoff=0.01, plot=False
     return weights_important
 
 
+def fivefoil_standard(mesh_3D, braid_func=braid, modes_cutoff=0.01, plot=False):
+    mesh_3D_new1 = rotate_meshgrid(*mesh_3D, np.radians(00), np.radians(00), np.radians(0))
+    mesh_3D_new2 = rotate_meshgrid(*mesh_3D, np.radians(00), np.radians(00), np.radians(0))
+    xyz_array = [
+        (mesh_3D_new1[0], mesh_3D_new1[1], mesh_3D_new1[2]),
+        (mesh_3D_new2[0], mesh_3D_new2[1], mesh_3D_new2[2])
+    ]
+    # starting angle for each braid
+    angle_array = np.array([0, 1. * np.pi])
+    # powers in cos in sin
+    pow_cos_array = [2.5, 2.5]
+    pow_sin_array = [2.5, 2.5]
+    # conjugating the braid (in "Milnor" space)
+    conj_array = [0, 0]
+    # moving x+iy (same as in the paper)
+    theta_array = [0.0 * np.pi, 0 * np.pi]
+    # braid scaling
+    a_cos_array = [1, 1]
+    a_sin_array = [1, 1]
+
+    ans = 1
+    for i, xyz in enumerate(xyz_array):
+        if conj_array[i]:
+            ans *= np.conjugate(braid_func(*xyz, angle_array[i], pow_cos_array[i], pow_sin_array[i], theta_array[i],
+                                           a_cos_array[i], a_sin_array[i]))
+        else:
+            ans *= braid_func(*xyz, angle_array[i], pow_cos_array[i], pow_sin_array[i], theta_array[i],
+                              a_cos_array[i], a_sin_array[i])
+    R = np.sqrt(mesh_3D[0] ** 2 + mesh_3D[1] ** 2)
+    ans *= (1 + R ** 2) ** 5
+    w = 0.85
+    ans *= LG_simple(*mesh_3D[:2], 0, l=0, p=0, width=w, k0=1, x0=0, y0=0, z0=0)
+
+    moments = {'p': (0, 6), 'l': (-6, 6)}
+
+    _, _, res_z_3D = np.shape(mesh_3D_new1[0])
+    x_2D = mesh_3D[0][:, :, 0]
+    y_2D = mesh_3D[1][:, :, 0]
+    if plot:
+        plot_field_both(ans[:, :, res_z_3D // 2])
+    values = cbs.LG_spectrum(
+        ans[:, :, res_z_3D // 2], **moments, mesh=(x_2D, y_2D), plot=True, width=w, k0=1,
+    )
+    l_save = []
+    p_save = []
+    weight_save = []
+    moment0 = moments['l'][0]
+    for l, p_array in enumerate(values):
+        for p, value in enumerate(p_array):
+            if abs(value) > modes_cutoff * abs(values).max():
+                l_save.append(l + moment0)
+                p_save.append(p)
+                weight_save.append(value)
+    weight_save /= np.sqrt(np.sum(np.array(weight_save) ** 2)) * 100
+    weights_important = {'l': l_save, 'p': p_save, 'weight': weight_save}
+    return weights_important
+
 def trefoil_optimized(mesh_3D, braid_func=braid, modes_cutoff=0.01, plot=False):
     l_save = [0, 0, 0, 0, 3]
     p_save = [0, 1, 2, 3, 0]
@@ -1473,7 +1530,7 @@ if __name__ == "__main__":
     # x_lim_3D_knot, y_lim_3D_knot, z_lim_3D_knot = (-10.0, 10.0), (-10.0, 10.0), (-2.0, 2.0)
     # x_lim_3D_knot, y_lim_3D_knot, z_lim_3D_knot = (-5.0, 5.0), (-5.0, 5.0), (-2.0, 2.0)
     res_x_3D_knot, res_y_3D_knot, res_z_3D_knot = 90, 90, 40
-    res_x_3D_knot, res_y_3D_knot, res_z_3D_knot = 190, 190, 3
+    # res_x_3D_knot, res_y_3D_knot, res_z_3D_knot = 190, 190, 3
     # res_x_3D_knot, res_y_3D_knot, res_z_3D_knot = 100, 100, 100
     if res_z_3D_knot != 1:
         z_3D_knot = np.linspace(*z_lim_3D_knot, res_z_3D_knot)
@@ -1492,7 +1549,7 @@ if __name__ == "__main__":
 
     # values = unknot_4_any(mesh_3D_knot, braid_func=braid, plot=True,
     #                       angle_size=(2, 2, 2, 1))
-    values = trefoil_optimized(mesh_3D_knot, braid_func=braid, plot=True)
+    values = fivefoil_standard(mesh_3D_knot, braid_func=braid, plot=True)
     field = field_knot_from_weights(
         values, mesh_3D_knot, width0, k0=k0, x0=0, y0=0, z0=z0
     )
